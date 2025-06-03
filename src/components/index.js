ca1b7691448874a62d747ba106d3a5ef0616cbd3
yss1235-why <cofclanone1@gmail.com>
2025-06-04 00:40:03 +0530

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,15 @@
-// File path: src/components/index.js - Component exports
-export { default as PrivacyPolicy } from './PrivacyPolicy';
-export { default as TermsOfService } from './TermsOfService';
+// File path: src/components/index.js - Updated Component exports
+export { 
+  PrivacyPolicy,
+  TermsOfService,
+  FAQ,
+  ResponsibleGambling,
+  LegalModal,
+  LegalFooter,
+  useConsentTracking,
+  LEGAL_VERSIONS,
+  COMPANY_INFO
+} from './LegalComponents';
+
+// Default export for convenience
+export { default as LegalModal } from './LegalComponents';
